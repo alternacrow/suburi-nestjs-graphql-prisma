a567a1790ceac292ea4b238f7f36b176c0fe4f4a
alternacrow <k.horita21@gmail.com>
2022-08-02 23:31:07 +0900

    Chore change database from postgresql to sqlite

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,25 +1,13 @@
-import {
-  ObjectType,
-  registerEnumType,
-  HideField,
-  Field,
-} from '@nestjs/graphql';
+import { ObjectType, HideField } from '@nestjs/graphql';
 import { Post } from 'src/posts/models/post.model';
 import { BaseModel } from 'src/common/models/base.model';
-import { Role } from '@prisma/client';
-
-registerEnumType(Role, {
-  name: 'Role',
-  description: 'User role',
-});
 
 @ObjectType()
 export class User extends BaseModel {
   email: string;
   firstname?: string;
   lastname?: string;
-  @Field(() => Role)
-  role: Role;
+  role: number;
   posts: Post[];
   @HideField()
   password: string;
